9772c6580803c67a7dcdc2e8ccddfc4d6c6e86cd
R.Grouls <raoul.grouls@businessdecision.nl>
2022-08-02 17:39:05 +0200

    cleanup

--- load.py
+++
@@ -1,9 +0,0 @@
-from dgl.data.utils import load_graphs
-
-def get_data(file = "tfinance"):
-    graph, label_dict = load_graphs(file)
-    graph = graph[0]
-    graph.ndata['label'] = graph.ndata['label'].argmax(1)
-    graph.ndata['label'] = graph.ndata['label'].long().squeeze(-1)
-    graph.ndata['feature'] = graph.ndata['feature'].float()
-    return graph
